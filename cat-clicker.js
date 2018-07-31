5156f7216fdeeb541d090bbf06c0d7c3abdf8a79
anita <anita.ani2007@gmail.com>
2018-07-31 10:21:48 -0700

    five cat code with each one's detail is being shown on clicking its name

--- cat-clicker.js
+++ cat-clicker.js
@@ -1,13 +1,30 @@
-let clickCountOne = 0;
-let clickCountTwo = 0;
-const clicksOne = document.querySelector('.clicksOne');
+let lastCatImage;
+let isLastCatImage = false;
+const clicksNameOne = document.querySelector('.Patchy-name');
 const targetImageOne = document.querySelector('#cat_image_one');
-const clicksTwo = document.querySelector('.clicksTwo');
+const clicksNameTwo = document.querySelector('.Whitty-name');
 const targetImageTwo = document.querySelector('#cat_image_two');
-let allClicks = [{target: targetImageOne, clickCount: 0},
-                 {target: targetImageTwo, clickCount: 0}];
+const clicksNameThree = document.querySelector('.Blacky-name');
+const targetImageThree = document.querySelector('#cat_image_three');
+const clicksNameFour = document.querySelector('.Tiger-name');
+const targetImageFour = document.querySelector('#cat_image_four');
+const clicksNameFive = document.querySelector('.Sweety-name');
+const targetImageFive = document.querySelector('#cat_image_five');
+const clicksImageOne = document.querySelector('#clicks-one');
+const clicksImageTwo = document.querySelector('#clicks-two');
+const clicksImageThree = document.querySelector('#clicks-three');
+const clicksImageFour = document.querySelector('#clicks-four');
+const clicksImageFive = document.querySelector('#clicks-five');
 
-function updateClickCount(e){
+let allCats = [
+		{name: clicksNameOne, image: "Patchy-image", target: targetImageOne, clickClass: clicksImageOne},
+        {name: clicksNameTwo, image: "Whitty-image", target: targetImageTwo, clickClass: clicksImageTwo},
+        {name: clicksNameThree, image: "Blacky-image", target: targetImageThree, clickClass: clicksImageThree},
+        {name: clicksNameFour, image: "Tiger-image", target: targetImageFour, clickClass: clicksImageFour},
+        {name: clicksNameFive, image: "Sweety-image", target: targetImageFive, clickClass: clicksImageFive}
+        ];
+
+/*function updateClickCount(e){
 	if(e.target === targetImageOne){
 		clickCountOne++;
 		clicksOne.innerHTML=clickCountOne;
@@ -17,11 +34,31 @@ function updateClickCount(e){
 		clicksTwo.innerHTML=clickCountTwo;
 	}
 }
+*/
 
-allClicks.forEach(function(eachClick){
-	eachClick.target.addEventListener('click', updateClickCount);
+allCats.forEach(function(cat){
+	let catNametoClick = cat.name;
+	catNametoClick.addEventListener('click', (function(newCat) {
+		return function() {
+		let clickTarget = newCat.target;
+		let catImage = newCat.image;
+		let catCountClass = newCat.clickClass;
+		let CatCount = parseInt(catCountClass.innerHTML);
+		if(isLastCatImage){
+			document.getElementById(lastCatImage).style.display = "none";
+		}
+		else{
+			isLastCatImage = true;
+		}
+		document.getElementById(catImage).style.display = "block";
+		lastCatImage = catImage;
+		clickTarget.addEventListener('click', (function(newCatCountClass, newCatCount) {
+			return function() {
+			newCatCount++;
+			newCatCountClass.innerHTML=newCatCount;
+			};
+		})(catCountClass, CatCount));
+	};
+})(cat));
 });
-//targetImageOne.addEventListener('click', updateClickCount(clickCountOne, targetImageOne));
-//targetImageTwo.addEventListener('click', updateClickCount(clickCountTwo, targetImageTwo));
-
 
