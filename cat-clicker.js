8b70e9b63dd001b29223b1dfb6285d8c408ddb4c
anita <anita.ani2007@gmail.com>
2018-08-07 12:12:31 -0700

    created array of objects to be used to display cats on the page

--- cat-clicker.js
+++ cat-clicker.js
@@ -1,5 +1,20 @@
 let lastCatImage;
 let isLastCatImage = false;
+const catCount = 5;
+const catDetail = [{name: "Patchy", src: "images/cat_image_one.jpg", ref: "Photo by zelle duda on Unsplash"},
+ 									 {name: "Whitty", src: "images/cat_image_two.jpg", ref: "Photo by Pixabay from pixabay.com"},
+									 {name: "Blacky", src: "images/cat_image_three.jpg", ref: "Photo by Chinda Sam on Unsplash"},
+									 {name: "Tiger", src: "images/cat_image_four.jpg", ref: "Photo by Clark Young on Unsplash"},
+									 {name: "Sweety", src: "images/cat_image_five.jp", ref: "Photo by Michael Sum on Unsplash"}]
+/*const imgSrc = ["images/cat_image_one.jpg", "images/cat_image_two.jpg",
+                "images/cat_image_three.jpg", "images/cat_image_four.jpg",
+							  "images/cat_image_five.jpg"];
+const imgRef = ["Photo by zelle duda on Unsplash", "Photo by Pixabay from pixabay.com",
+								"Photo by Chinda Sam on Unsplash", "Photo by Clark Young on Unsplash",
+								"Photo by Michael Sum on Unsplash"];
+const catName = ["Patchy", "Whitty", "Blacky", "Tiger", "Sweety"];*/
+const imageList = document.querySelector('.image-list');
+const images = document.querySelector('.images');
 const clicksNameOne = document.querySelector('.Patchy-name');
 const targetImageOne = document.querySelector('#cat_image_one');
 const clicksNameTwo = document.querySelector('.Whitty-name');
@@ -16,6 +31,37 @@ const clicksImageThree = document.querySelector('#clicks-three');
 const clicksImageFour = document.querySelector('#clicks-four');
 const clicksImageFive = document.querySelector('#clicks-five');
 
+for (let i = 0; i < catCount; i++) {
+		let element = document.createElement('li');
+		element.setAttribute("id","cat"+i);
+		element.innerText = catDetail[i].name;
+		imageList.appendChild(element);
+}
+for (let i = 0; i < catCount; i++) {
+		let elementFigure = document.createElement('figure');
+		elementFigure.setAttribute("id","image"+i);
+		let elementChild = document.createElement('h3');
+		elementChild.setAttribute("class","catName"+i);
+		elementChild.innerText = catDetail[i].name;
+		elementFigure.appendChild(elementChild);
+		/*elementChild = document.createElement('img');
+		elementChild.setAttribute("id","catImage"+i);
+		elementChild.innerText = "cat"+i;
+		imageList.appendChild(elementChild);
+		elementChild = document.createElement('figcaption');
+		elementChild.setAttribute("id","cat"+i);
+		elementChild.innerText = "cat"+i;
+		imageList.appendChild(elementChild);
+		elementChild = document.createElement('h2');
+		elementChild.setAttribute("id","cat"+i);
+		elementChild.innerText = "cat"+i;
+		imageList.appendChild(elementChild);
+		elementChild = document.createElement('div');
+		elementChild.setAttribute("id","cat"+i);
+		elementChild.innerText = "cat"+i;
+		imageList.appendChild(elementChild);*/
+		images.appendChild(elementFigure);
+}
 let allCats = [
 		{name: clicksNameOne, image: "Patchy-image", target: targetImageOne, clickClass: clicksImageOne},
         {name: clicksNameTwo, image: "Whitty-image", target: targetImageTwo, clickClass: clicksImageTwo},
@@ -35,7 +81,7 @@ let allCats = [
 	}
 }
 */
-
+/*
 allCats.forEach(function(cat){
 	let catNametoClick = cat.name;
 	catNametoClick.addEventListener('click', (function(newCat) {
@@ -61,3 +107,4 @@ allCats.forEach(function(cat){
 	};
 })(cat));
 });
+*/
